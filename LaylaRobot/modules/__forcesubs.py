4dfdad8108b5a1da703e7d5080e81d3f5e7a0362
RAVANOFFICIAL <92657138+KINGOFASURA@users.noreply.github.com>
2021-10-22 04:45:18 +0530

    Update __forcesubs.py

--- LaylaRobot/modules/__forcesubs.py
+++ LaylaRobot/modules/__forcesubs.py
@@ -12,9 +12,10 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from LaylaRobot import DRAGONS as SUDO_USERS
-from LaylaRobot import pbot
-from LaylaRobot.modules.sql_extended import forceSubscribe_sql as sql
+from XROBOT import DRAGONS as SUDO_USERS
+from XROBOT import pbot
+from XROBOT.modules.sql_extended import forceSubscribe_sql as sql
+
 
 logging.basicConfig(level=logging.INFO)
 
